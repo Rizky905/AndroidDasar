1b530375c1b371044fc774ad08ecfa8155f132cb
Rizky905 <erlangga905@gmail.com>
2020-05-14 14:23:10 +0700

    commits one

--- app/src/main/java/com/example/androiddasar/MainActivity.kt
+++ app/src/main/java/com/example/androiddasar/MainActivity.kt
@@ -8,11 +8,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test
-
-        
-
-
 
     }
+
+    override fun onPause() {
+        super.onPause()
+        //onPause adalah fungsi yang akan dipanggil ketika user menekan tombol home
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // onResume adalah fungsi yang akan dipanggil ketika user akan meng-click kembali setelah
+        // menekan tombol home
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // onDestroy adalah fungsi yang akan dipanggil ketika user menekan tombol back
+    }
+
 }
