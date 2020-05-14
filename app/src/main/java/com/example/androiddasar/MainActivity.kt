9c6f68370de30909dc10fadcaa1e699c5db64164
Rizky905 <erlangga905@gmail.com>
2020-05-14 15:07:43 +0700

    commits one

--- app/src/main/java/com/example/androiddasar/MainActivity.kt
+++ app/src/main/java/com/example/androiddasar/MainActivity.kt
@@ -2,17 +2,14 @@ package com.example.androiddasar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test
 
-        
-
-
-
+        supportFragmentManager.beginTransaction().replace(R.id.fl_main,MainFragment()).commit()
     }
 }
